392e27045731d00e9636a5cd06052aa075f6df1c
aviany <ayelite@gmail.com>
2017-08-28 17:14:22 +0200

    Baseline for Love

---
+++ main.lua
@@ -0,0 +1,13 @@
+-- Narejeno na verziji Löve 0.10.2
+
+function love.load()
+end
+
+function love.update( dt )
+end
+
+function love.draw()
+end
+
+function love.keypressed( key, scancode, isrepeat )
+end
