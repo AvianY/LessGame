99ea6038fe11154f52f68bc32c225c2c355d56ee
aviany <ayelite@gmail.com>
2017-08-29 23:59:24 +0200

    Added middle mouse camera translation

--- main.lua
+++ main.lua
@@ -89,12 +89,21 @@ function love.load()
 	game.size = 200
 	game.whiteColor = { r = 245, g = 222, b = 179 }
 	game.blackColor = { r = 139, g = 69, b = 19 }
+	xmidPressed = 0
+	ymidPressed = 0
+	xmouse = 0
+	ymouse = 0
 end
 
 function love.update( dt )
+	if love.mouse.isDown( 3 ) then
+		xmouse, ymouse = love.mouse.getPosition()
+	end
 end
 
 function love.draw()
+	love.graphics.translate( xmouse - xmidPressed, ymouse - ymidPressed)
+
 	drawBlk( 0, 0, game.size, blk.TB, 3)
 	drawBlk( game.size, 0, game.size, blk.ZZ, 3)
 	drawBlk( 0, game.size, game.size, blk.ZZ, 2)
@@ -116,5 +125,17 @@ end
 function love.keypressed( key, scancode, isrepeat )
 	if scancode == 'q' then
 		love.event.quit()
+	end
+end
+
+function love.mousepressed( x, y, button, istouch)
+	if button == 1 then
+		print("to je 1")
+	end
+	if button == 2 then
+		print("to je 2")
+	end
+	if button == 3 then
+		xmidPressed,ymidPressed = love.mouse.getPosition()
 	end
 end
